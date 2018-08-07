01c6a8d8ad685ce7649b815ee188e0a09f615a71
smadari <shishir_madari@gap.com>
2018-08-06 18:31:06 -0700

    directory updates, importing Material-UI, creating Header.js. most boilerplate still here

--- e-raffle/src/index.js
+++ e-raffle/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import RafflePage from './workflows/RafflePage/RafflePage';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RafflePage />, document.getElementById('root'));
 registerServiceWorker();
--- e-raffle/src/workflows/RafflePage/RafflePage.js
+++ e-raffle/src/workflows/RafflePage/RafflePage.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './../../logo.svg';
+import './../../App.css';
 
-class App extends Component {
+// components
+import Header from './../../components/header/Header';
+
+
+class RafflePage extends Component {
   render() {
     return (
       <div className="App">
@@ -10,6 +14,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,4 +23,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default RafflePage;
